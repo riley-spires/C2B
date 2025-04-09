01c62ede07f3104a85cb1c91ed2ea54ff0d9217d
Riley Spires <rjs243@uakron.edu>
2025-04-09 15:08:33 -0400

    Super basic logger example

---
+++ examples/logger/main.cpp
@@ -0,0 +1,18 @@
+#include "../../c2b.h"
+#include <fstream>
+
+
+int main(int argc, char** argv) {
+    c2b::Build::rebuild_self(argc, argv, __FILE__);
+
+    std::ofstream log("log.txt");
+
+    c2b::Logger logger(log);
+
+    std::cout << "Hello, World!" << std::endl;
+    logger.log_info("Outputted 'Hello, World!'") << std::endl;
+
+    log.close();
+
+    return 0;
+}
